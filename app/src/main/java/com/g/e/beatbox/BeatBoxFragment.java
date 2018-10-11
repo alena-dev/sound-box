9658be36d3c04aed1f9c32e1d5ae7a3dc6573ec3
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-11 19:45:12 +0300

    Task-1. Implement fragment and base classes

---
+++ app/src/main/java/com/g/e/beatbox/BeatBoxFragment.java
@@ -0,0 +1,10 @@
+package com.g.e.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment createInstance(){
+        return new BeatBoxFragment();
+    }
+}
