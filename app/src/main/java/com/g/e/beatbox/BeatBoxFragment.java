5728478adca93754b4f4de82393d50eba7268a0a
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-12 11:45:26 +0300

    Task-1. Bind layout to fragment

--- app/src/main/java/com/g/e/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/g/e/beatbox/BeatBoxFragment.java
@@ -1,10 +1,28 @@
 package com.g.e.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.g.e.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment createInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
     }
 }
