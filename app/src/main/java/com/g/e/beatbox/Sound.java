5a4faeba1c9a81743f53b41d3ebce013d0c9a931
alena <49859442+alena-dev@users.noreply.github.com>
2018-10-15 10:23:09 +0300

    Task-2. Use SoundPool and load sounds

--- app/src/main/java/com/g/e/beatbox/Sound.java
+++ app/src/main/java/com/g/e/beatbox/Sound.java
@@ -3,12 +3,13 @@ package com.g.e.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
-    public Sound(String assetPath){
-        mAssetPath=assetPath;
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
         String[] components = assetPath.split("/");
-        String fileName = components[components.length-1];
-        mName=fileName.replace(".wav","");
+        String fileName = components[components.length - 1];
+        mName = fileName.replace(".wav", "");
     }
 
     public String getAssetPath() {
@@ -17,5 +18,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
