0d9fa464b0e4b2919ef972dde5e597447bf4337b
alena <49859442+alena-dev@users.noreply.github.com>
2019-05-19 08:00:05 -0400

    Add main functionality

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -32,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String answer = QuestionHelper.getAnswer();
+                QuestionHelper helper = new QuestionHelper(2, 2);
+                String answer = Integer.toString(helper.getAnswer());
+
                 Toast toast = Toast.makeText(MainActivity.this, answer, Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.TOP, 0, 0);
                 toast.show();
--- app/src/main/java/com/example/testapp/QuestionHelper.java
+++ app/src/main/java/com/example/testapp/QuestionHelper.java
@@ -2,8 +2,17 @@ package com.example.testapp;
 
 public class QuestionHelper {
 
+    private int mFirstArg;
+    private int mSecondArg;
 
-    public static String getAnswer() {
-        return "4";
+    public QuestionHelper(int firstArg, int secondArg) {
+        mFirstArg = firstArg;
+        mSecondArg = secondArg;
+    }
+
+
+    public int getAnswer() {
+       int result = mFirstArg + mSecondArg;
+       return result;
     }
 }
